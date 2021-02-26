28a412ea4f7266000e9bf4bba90c34df3cd3f4cb
Logan Demmy <frank.demmy@gmail.com>
2021-02-26 12:43:29 -0600

    till the field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,9 @@ import { createAsparagus } from "./seeds/asparagus.js"
 
 const asparagusSeed = createAsparagus()
 console.log(asparagusSeed)
+
+import { addPlant, usePlants } from "./field.js"
+
+const seed = addPlant();
+const plant = usePlants(seed);
+console.log(plant)
